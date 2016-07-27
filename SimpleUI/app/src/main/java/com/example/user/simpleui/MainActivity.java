af039854bc836692d62f0ff7bda73fbc4ed1a924
cv86092 <cv86092@gmail.com>
2016-07-27 12:12:33 +0800

    Totaly complicate

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -4,19 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    Spinner spinner;
 
-    String sex ="male";
+    String drink ="black tea";
+    ListView listView;
 
-
+    List<Order> orders = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +36,16 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.maleRadioButton)
-                {
-                    sex = "male";
-                }
-                else if (checkedId == R.id.femaleRadioButton)
-                {
-                    sex = "female";
-                }
-
+                RadioButton radioButton = (RadioButton)group.findViewById(checkedId);
+                drink = radioButton.getText().toString();
             }
         });
 
@@ -52,14 +60,45 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        setupListView();
+        setupSpinner();
     }
 
+    private void setupListView(){
+
+//        String[] data = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12"};
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,orders);
+
+        OrderAdapter adapter = new OrderAdapter(this,orders);
+        listView.setAdapter(adapter);
+
+
+    }
+    private void setupSpinner(){
+        String[] data = getResources().getStringArray(R.array.storeinfos);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,data);
+        spinner.setAdapter(adapter);
+
+
+
+    }
     public void submit(View view)
     {
-
         String text = editText.getText().toString();
-        text = text + "  性別:" + sex;
+        text = text + "  order:" + drink;
         textView.setText(text);
         editText.setText("");
+
+        Order order = new Order();
+        order.note = text;
+        order.drink = drink;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+
+        orders.add(order);
+        setupListView();
+
+
     }
 }
